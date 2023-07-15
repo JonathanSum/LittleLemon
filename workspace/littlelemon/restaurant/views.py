e2cf8a07e19113095be56843f4f57aa69075bd47
Jonathan Sum <777JonathanSum@gmail.com>
2023-07-14 22:51:41 -0700

    update
    update

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from .models import MenuItem, Booking
-from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
+from .serializers import MenuItemSerializer, BookingSerializer
+# , UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import serializers
