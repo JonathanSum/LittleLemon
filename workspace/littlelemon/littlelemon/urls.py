a74a039943bc1a2b351241876999a596317fc6ac
Jonathan Sum <777JonathanSum@gmail.com>
2023-07-14 22:35:27 -0700

    update
    update

--- workspace/littlelemon/littlelemon/urls.py
+++ workspace/littlelemon/littlelemon/urls.py
@@ -22,7 +22,7 @@ from restaurant import views
 router = routers.DefaultRouter()
 
 router.register(r'tables', views.BookingViewSet)
-router.register(r'users', views.UserViewSet)
+# router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
