85d74a6713442b2d26d30ae52593a4033f107c4d
Jonathan Sum <777JonathanSum@gmail.com>
2023-07-14 22:34:15 -0700

    update
    update

--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -1,6 +1,8 @@
 #define URL route for index() view
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
+# , include
+# from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
@@ -11,4 +13,5 @@ urlpatterns = [
     path('menu-items/', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     # path('message/', views.msg),
+     path('api-token-auth/', obtain_auth_token),
 ]
