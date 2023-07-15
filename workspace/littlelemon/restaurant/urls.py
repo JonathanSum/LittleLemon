3cec8393b627155cffdb61096c275e35229a3ad1
Jonathan Sum <777JonathanSum@gmail.com>
2023-07-14 22:25:22 -0700

    update
    update

--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu-items/', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('message/', views.msg),
+    # path('message/', views.msg),
 ]
